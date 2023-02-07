5e583df86ad4b711432dc5ce15d0b5ebfd3f1fe3
SudarshanMSGit <103193886+SudarshanMSGit@users.noreply.github.com>
2023-02-07 17:59:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    trailingSlash: true
-};
+    async headers() {
+      return [
+        {
+          source: '/',
+          headers: [
+            {
+              key: 'x-custom-header',
+              value: 'my custom header value',
+            },
+            {
+              key: 'x-another-custom-header',
+              value: 'my other custom header value',
+            },
+          ],
+        },
+      ]
+    },
+  };
+  
